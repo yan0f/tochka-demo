42f690feb3b15c664a418296f927d552b0cbafbd
yan0f <31446439+yan0f@users.noreply.github.com>
2025-05-26 14:53:24 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,91 @@
+# /// script
+# requires-python = ">=3.11"
+# dependencies = [
+#     "requests",
+# ]
+# ///
+import csv
+
+import requests
+
+RS_URL = 'https://enter.tochka.com/sandbox/v2'
+
+API_VERSION = 'v1.0'
+
+ACCOUNT_ID = '12345123451234512345/044525104'
+
+STATEMENT_ID = '3de271da-12a7-4d71-9c13-7878d87fb12f'
+
+URL = f'{RS_URL}/open-banking/{API_VERSION}/accounts/{ACCOUNT_ID}/statements/{STATEMENT_ID}'
+
+TOKEN = 'working_token' # noqa: S105
+
+HEADERS = {
+    'Authorization': f'Bearer {TOKEN}',
+}
+
+
+def fetch_data(url: str) -> dict:
+    response = requests.get(url, headers=HEADERS, timeout=10)
+    response.raise_for_status()
+    return response.json()
+
+
+def parse_transactions(data: dict) -> list[dict]:
+    transactions = []
+    statements = data.get('Data', {}).get('Statement', [])
+
+    for statement in statements:
+        for transaction in statement.get('Transaction', []):
+            transaction_id = transaction.get('transactionId')
+            amount = transaction.get('Amount', {}).get('amount')
+            currency = transaction.get('Amount', {}).get('currency')
+            date = transaction.get('documentProcessDate')
+            description = transaction.get('description')
+            direction = transaction.get('creditDebitIndicator')
+
+            if direction == 'Debit':
+                counterparty = transaction.get('CreditorParty', {}).get('name')
+            else:
+                counterparty = transaction.get('DebtorParty', {}).get('name')
+
+            transactions.append(
+                {
+                    'transaction_id': transaction_id,
+                    'amount': amount,
+                    'currency': currency,
+                    'date': date,
+                    'counterparty': counterparty,
+                    'transaction_description': description,
+                    'transaction_direction': direction,
+                }
+            )
+
+    return transactions
+
+
+def write_to_csv(transactions, filename: str) -> None:
+    fieldnames = [
+        'transaction_id',
+        'amount',
+        'currency',
+        'date',
+        'counterparty',
+        'transaction_description',
+        'transaction_direction',
+    ]
+    with open(filename, mode='w', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writeheader()
+        for transaction in transactions:
+            writer.writerow(transaction)
+
+
+def main():
+    data = fetch_data(URL)
+    transactions = parse_transactions(data)
+    write_to_csv(transactions, 'transactions.csv')
+
+
+if __name__ == '__main__':
+    main()
